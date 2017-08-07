6dc53d89b317ec678f26fda15db8e693268876e3
Rahul Das <rahuld707@gmail.com>
2017-08-07 10:21:19 +0530

    updated the test file

--- test/test.js
+++ test/test.js
@@ -1,16 +1,104 @@
-const assert=require("assert");
-const main=require("../scripts/main")
-describe("Converter",function(){
-   it("is one hundred twenty five",function(){
-        var test=main.factor("one hundred twenty five",0);
-        assert.equal(test,125);
-    });
-    it("should return 22",function(){
-        var test=main.findNum("twenty two",0,0);
-        assert.equal(test,22);
-    });
-    it("can parse sentences",function(){
-        var test=main.parseSentence("There are three hundred thousand four hundred and twenty six people residing in a total of sixty six countries");
-        assert.equal(test,"There are 300426 people residing in a total of 66 countries");
-    });
+describe("Number<-->String converter",function(){
+ describe("testing for factor",function(){
+      it("non-canonical numbers",function(){
+            var test1=factor("two hundred and thirty two");
+            expect(test1).toBe(232);
+            var test2=factor("six hundred and six");
+            expect(test2).toBe(606);
+            var test3=factor("nine hundred and seventeen");
+            expect(test3).toBe(917);
+            var test4=factor("twenty one");
+            expect(test4).toBe(21);
+        });
+      it("cardinal numbers without append",function(){
+            var test1=factor("two hundred and thirty first");
+            expect(test1).toBe(231);
+            var test2=factor("six hundred and sixth");
+            expect(test2).toBe(606);
+            var test3=factor("nine hundred and seventeenth");
+            expect(test3).toBe(917);
+            var test4=factor("twenty fourth");
+            expect(test4).toBe(24);
+        });  
+  });
+  describe("testing for findNum",function(){
+      it("non-cardinal numbers",function(){
+           var test1=findNum("one million two hundred thousand six hundred and fifty two",0,1,"");
+           expect(test1).toBe(1200652);
+           var test2=findNum("six million six hundred and twelve thousand six hundred and fifty two",0,1,"");
+           expect(test2).toBe(6612652);
+           var test3=findNum("six million twelve thousand six hundred and fifty two",0,1,"");
+           expect(test3).toBe(6012652);
+           var test4=findNum("twenty one quintillion eleven quadrillion nine trillion sixteen billion twelve million one hundred and twelve thousand nine hundred and ten",0,1,"");
+           expect(test4).toBe(21011009016012112910);
+           var test5=findNum("one hundred and sixty one",0,1,"");
+           expect(test5).toBe(161);
+      });
+      it("cardinal numbers",function(){
+           var test1=findNum("one million two hundred thousand six hundred and fifty second",0,1,"");
+           expect(test1).toBe("1200652nd");
+          var test2=findNum("six million six hundred and twelve thousand six hundred and seventeenth",0,1,"");
+           expect(test2).toBe("6612617th");
+           var test3=findNum("six million twelve thousand six hundred and seventeenth",0,1,"");
+           expect(test3).toBe("6012617th");
+           var test4=findNum("twenty one million six hundred thousand and fifty sixth",0,1,"");
+           expect(test4).toBe("21600056th");
+           var test4=findNum("twenty first",0,1,"");
+           expect(test4).toBe("21st");
+      });
+  }); 
+ describe("testing for findStr",function(){
+   it("non-cardinal numbers",function(){
+     var test1=findStr("1200652");
+      expect(test1).toBe("one million two hundred thousand six hundred and fifty two");
+      var test2=findStr("11200616");
+      expect(test2).toBe("eleven million two hundred thousand six hundred and sixteen");
+      var test3=findStr("1024");
+      expect(test3).toBe("one thousand and twenty four");
+      var test4=findStr("610");
+      expect(test4).toBe("six hundred and ten");
+      var test5=findStr("106");
+      expect(test5).toBe("one hundred and six");
+      var test6=findStr("1000010");
+      expect(test6).toBe("one million ten");
+   }); 
+   it("cardinal numbers",function(){
+      var test1=findStr("1200652nd");
+      expect(test1).toBe("one million two hundred thousand six hundred and fifty second");
+      var test2=findStr("11200616th");
+      expect(test2).toBe("eleven million two hundred thousand six hundred and sixteenth");
+      var test2=findStr("1012th");
+      expect(test2).toBe("one thousand and twelvth");
+   });  
+ });  
+ describe("testing for parsedStr",function(){
+    it("non-cardinal strings",function(){
+      var test1=parseStr("This is twenty one");
+      expect(test1).toBe("This is 21");
+      var test2=parseStr("There are two hundred thousand six hundred and ten classes and one million sixteen hundred students in this university");
+      expect(test2).toBe("There are 200610 classes and 1001600 students in this university");
+      var test3=parseStr("His score is two thousand");
+      expect(test3).toBe("His score is 2000");
+     });
+     it("cardinal strings",function(){
+      var test1=parseStr("This is twenty first");
+      expect(test1).toBe("This is 21st");
+     var test2=parseStr("He came third in the class out of three hundred and sixty students");
+      expect(test2).toBe("He came 3rd in the class out of 360 students");
+     });
+ });
+ describe("testing for parsedNum",function(){
+     it("non-cardinal strings",function(){
+         var test1=parseNum("My age is 23");
+         expect(test1).toBe("My age is twenty three");
+         var test2=parseNum("There are 1020100015 stars in the sky");
+         expect(test2).toBe("There are one billion twenty million one hundred thousand and fifteen stars in the sky");
+     });
+     it("cardinal strings",function(){
+         var test1=parseNum("This is 21st century");
+         expect(test1).toBe("This is twenty first century");
+         var test2=parseNum("This is the 1020100015th star in the sky");
+         expect(test2).toBe("This is the one billion twenty million one hundred thousand and fifteenth star in the sky");
+     });
+ }); 
 });
